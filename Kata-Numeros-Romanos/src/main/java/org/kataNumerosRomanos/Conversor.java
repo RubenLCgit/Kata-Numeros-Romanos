430cdd0d74df3785db845fdb01dd8e39c88759b2
RubenLCgit <125395496+RubenLCgit@users.noreply.github.com>
2023-03-14 19:01:28 +0100

    test: add tests and functions for cases 4 to 9

--- Kata-Numeros-Romanos/src/main/java/org/kataNumerosRomanos/Conversor.java
+++ Kata-Numeros-Romanos/src/main/java/org/kataNumerosRomanos/Conversor.java
@@ -3,7 +3,13 @@ package org.kataNumerosRomanos;
 public class Conversor {
     public String convert(int number){
         String conerversion;
-        if (number==3) conerversion = "III";
+        if (number==9) conerversion = "IX";
+        else if (number==8) conerversion = "VIII";
+        else if (number==7) conerversion = "VII";
+        else if (number==6) conerversion = "VI";
+        else if (number==5) conerversion = "V";
+        else if (number==4) conerversion = "IV";
+        else if (number==3) conerversion = "III";
         else if (number==2) conerversion = "II";
         else conerversion = "I";
         return conerversion;
--- Kata-Numeros-Romanos/src/test/java/TestConversor.java
+++ Kata-Numeros-Romanos/src/test/java/TestConversor.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestConversor {
     @ParameterizedTest
-    @CsvSource({ "1,I", "2,II", "3,III" })
-    void convert_number_to_FizzBuzz_string(int input, String expectedOutput) {
+    @CsvSource({ "1,I", "2,II", "3,III", "4,IV", "5,V", "6,VI", "7,VII", "8,VIII", "9,IX" })
+    void convert_number_to_Roman_string(int input, String expectedOutput) {
         assertEquals(expectedOutput, new Conversor().convert(input));
     }
 }
